2052dedb41bdff61e4048f280b123a160927beae
Scott Corio <scott.corio@bausch.com>
2024-04-19 15:33:19 -0500

    Add 4.01inF display Support

--- esphome/components/waveshare_epaper/display.py
+++ esphome/components/waveshare_epaper/display.py
@@ -50,6 +50,9 @@ WaveshareEPaper2P9InV2R2 = waveshare_epaper_ns.class_(
     "WaveshareEPaper2P9InV2R2", WaveshareEPaper
 )
 GDEY029T94 = waveshare_epaper_ns.class_("GDEY029T94", WaveshareEPaper)
+WaveshareEPaper4P01InF = waveshare_epaper_ns.class_(
+    "WaveshareEPaper4P01InF", WaveshareEPaper7C
+)
 WaveshareEPaper4P2In = waveshare_epaper_ns.class_(
     "WaveshareEPaper4P2In", WaveshareEPaper
 )
@@ -119,6 +122,7 @@ MODELS = {
     "2.90in-b": ("b", WaveshareEPaper2P9InB),
     "2.90in-bv3": ("b", WaveshareEPaper2P9InBV3),
     "2.90inv2-r2": ("c", WaveshareEPaper2P9InV2R2),
+    "4.01in-f": ("b", WaveshareEPaper4P01InF),
     "4.20in": ("b", WaveshareEPaper4P2In),
     "4.20in-bv2": ("b", WaveshareEPaper4P2InBV2),
     "5.83in": ("b", WaveshareEPaper5P8In),
